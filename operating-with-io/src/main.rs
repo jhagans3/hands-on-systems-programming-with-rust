f5399781476bab09483ac0fc3ebbcab984973573
James Hagans <jhagans@acm.org>
2020-05-28 16:55:10 -0400

    Read from stdin, Count Bytes, and Write to stdout

--- operating-with-io/src/main.rs
+++ operating-with-io/src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{self, Read, Write};
+
+const CHUNK_SIZE: usize = 16 * 1024;
+
 fn main() {
-    println!("Hello, world!");
+// dd if=/dev/urandom bs=1024 count=128 of=myfile
+// cat myfile | target/debug/pipeviewer > myfile2
+    let mut total_bytes = 0;
+    loop {
+        let mut buffer = [0; CHUNK_SIZE];
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+
+        total_bytes += num_read;
+
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+    eprintln!("total bytes: {}", total_bytes);
 }
