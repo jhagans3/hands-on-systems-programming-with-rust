1912b714aee1dbe89523eef948ac9c50d12a39ed
James Hagans <jhagans@acm.org>
2020-05-28 17:37:42 -0400

    Handling Errors Gracefully

--- operating-with-io/src/main.rs
+++ operating-with-io/src/main.rs
@@ -1,14 +1,15 @@
-use std::io::{self, Read, Write};
+use std::io::{self, ErrorKind, Read, Result, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() {
+fn main() -> Result<()> {
     // dd if=/dev/urandom bs=1024 count=128 of=myfile
     // cat myfile | target/debug/pipeviewer > myfile2
+
     let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
 
     loop {
-        let mut buffer = [0; CHUNK_SIZE];
         let num_read = match io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
@@ -17,10 +18,27 @@ fn main() {
 
         total_bytes += num_read;
 
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        eprintln!("{}", total_bytes);
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+            // yes | cargo run | head -n 1 > /dev/null
+            // Error: Os { code: 32, kind: BrokenPipe, message: "Broken pipe" }
+
+            // skip broken pipe error
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+
+            // yes | cargo run | head -n 2 > /dev/null
+            // Oh no, an error! Broken pipe (os error 32)
+            // eprint!("Oh no, an error! {}\n", e.to_string());
+            // std::process::exit(1);
+        }
     }
 
-    eprintln!("total bytes: {}", total_bytes);
+    eprintln!("{}", total_bytes);
+
+    Ok(())
 }
 
 /*
