5f898135f44f1100f0716e0ef7d60d8bbecab916
James Hagans <jhagans@acm.org>
2020-05-28 17:12:10 -0400

    add git pre-commit

--- operating-with-io/src/main.rs
+++ operating-with-io/src/main.rs
@@ -3,9 +3,10 @@ use std::io::{self, Read, Write};
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
-// dd if=/dev/urandom bs=1024 count=128 of=myfile
-// cat myfile | target/debug/pipeviewer > myfile2
+    // dd if=/dev/urandom bs=1024 count=128 of=myfile
+    // cat myfile | target/debug/pipeviewer > myfile2
     let mut total_bytes = 0;
+
     loop {
         let mut buffer = [0; CHUNK_SIZE];
         let num_read = match io::stdin().read(&mut buffer) {
@@ -18,5 +19,13 @@ fn main() {
 
         io::stdout().write_all(&buffer[..num_read]).unwrap();
     }
+
     eprintln!("total bytes: {}", total_bytes);
 }
+
+/*
+.git/hooks/pre-commit
+
+cargo fmt
+exec cargo clippy -- -D warnings
+*/
