d32ec8da84eeb640d426f8bbb14d3c41e83c815a
James Hagans <jhagans@acm.org>
2020-05-28 18:20:22 -0400

    Handling Command Line Arguments with structopt

--- operating-with-io/src/main.rs
+++ operating-with-io/src/main.rs
@@ -1,10 +1,31 @@
 use std::io::{self, ErrorKind, Read, Result, Write};
+use structopt::StructOpt;
 
 const CHUNK_SIZE: usize = 16 * 1024;
+
+// cargo run -- --help
+#[derive(StructOpt, Debug)]
+// #[structopt(name = "basic")]
+struct Opt {
+    /// Read from a file instead of stdin
+    #[structopt()]
+    infile: Option<String>,
+
+    /// Write output to a file instead of stdout
+    #[structopt(short, long)]
+    outfile: Option<String>,
+
+    /// Display total bytes
+    #[structopt(short, long)]
+    silent: bool,
+}
 
 fn main() -> Result<()> {
     // dd if=/dev/urandom bs=1024 count=128 of=myfile
     // cat myfile | target/debug/pipeviewer > myfile2
+
+    let args = Opt::from_args();
+    let silent = args.silent;
 
     let mut total_bytes = 0;
     let mut buffer = [0; CHUNK_SIZE];
@@ -18,7 +39,10 @@ fn main() -> Result<()> {
 
         total_bytes += num_read;
 
-        eprintln!("{}", total_bytes);
+        if !silent {
+            eprintln!("{}", total_bytes);
+        }
+
         if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
             // yes | cargo run | head -n 1 > /dev/null
             // Error: Os { code: 32, kind: BrokenPipe, message: "Broken pipe" }
@@ -36,7 +60,9 @@ fn main() -> Result<()> {
         }
     }
 
-    eprintln!("{}", total_bytes);
+    if !silent {
+        eprintln!("total bytes: {}", total_bytes);
+    }
 
     Ok(())
 }
