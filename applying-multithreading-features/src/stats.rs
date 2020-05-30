6c336a58612d8d7cdfadf2fb67a37192c9f39feb
James Hagans <jhagans@acm.org>
2020-05-30 01:02:01 -0400

    Working with Instant

--- applying-multithreading-features/src/stats.rs
+++ applying-multithreading-features/src/stats.rs
@@ -1,14 +1,16 @@
 use crossbeam::Receiver;
 use std::io::Result;
+use std::time::Instant;
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
+    let start = Instant::now();
 
     loop {
         let num_bytes = stats_rx.recv().unwrap();
         total_bytes += num_bytes;
         if !silent {
-            eprint!("\r{}", total_bytes);
+            eprint!("\r{} {}", total_bytes, start.elapsed().as_secs());
         }
 
         if num_bytes == 0 {
